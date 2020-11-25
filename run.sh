ca4fc07e399c717cc3328a2533519ad6aaa2fe07
Mahyar Samani <msamani@ucdavis.edu>
2020-11-25 10:28:29 -0800

    Updated run.sh.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 for BW in $(seq 5 10 300)
     do for paging in open close close_adaptive
         do
-        gem5/build/NULL/gem5.opt --outdir=results2/LINEAR/LLM_$paging/BW_$BW -re configs-test-llm/run_llm_eval.py LLM 1 0 60 $paging 1 LINEAR 1us $BW 100 0
+        gem5/build/NULL/gem5.opt --outdir=results/LINEAR/LLM_$paging/BW_$BW -re configs-test-llm/run_llm_eval.py LLM 1 0 60 $paging 1 LINEAR 1us $BW 100 0
     done
-    gem5/build/NULL/gem5.opt --outdir=results2/LINEAR/HBM/BW_$BW -re configs-test-llm/run_llm_eval.py HBM 1 0 60 open 1 LINEAR 1us $BW 100 0
+    gem5/build/NULL/gem5.opt --outdir=results/LINEAR/HBM/BW_$BW -re configs-test-llm/run_llm_eval.py HBM 1 0 60 open 1 LINEAR 1us $BW 100 0
 done
